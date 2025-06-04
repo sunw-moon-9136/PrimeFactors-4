c3d6c8483e2022756cc8ecc0c281fce85558e4df
sunw.moon <sunw.moon@samsung.com>
2025-06-04 10:38:12 +0900

    [feature] 2p

--- PrimeFactor/src/test/java/PrimeFactorTest.java
+++ PrimeFactor/src/test/java/PrimeFactorTest.java
@@ -15,4 +15,24 @@ class PrimeFactorTest {
 
         assertEquals(expected, ret);
     }
+
+    @Test
+    void getPrimeFactorsFrom72() {
+        PrimeFactor primeFactor = new PrimeFactor(72);
+        List<Integer> expected = List.of(2, 2, 2, 3, 3);
+
+        List<Integer> ret = primeFactor.getPrimeFactors();
+
+        assertEquals(expected, ret);
+    }
+
+    @Test
+    void getPrimeFactorsFrom100() {
+        PrimeFactor primeFactor = new PrimeFactor(100);
+        List<Integer> expected = List.of(2, 2, 5, 5);
+
+        List<Integer> ret = primeFactor.getPrimeFactors();
+
+        assertEquals(expected, ret);
+    }
 }
